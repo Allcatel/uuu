775d3f785a32e4a4972f06b5d797c6cb01bd326c
Allcatel <br.al@i.ua>
2016-11-04 04:07:18 +0200

    Add files via upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.table2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(12, 67);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(392, 322);
+            this.table.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(137, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "start.txt";
+            // 
+            // table2
+            // 
+            this.table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table2.Location = new System.Drawing.Point(445, 67);
+            this.table2.Name = "table2";
+            this.table2.Size = new System.Drawing.Size(405, 322);
+            this.table2.TabIndex = 7;
+            this.table2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(94, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.table2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView table2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Factor
+        {
+            int k;
+            int n;
+            double rangs;
+
+            List<List<int>> list = new List<List<int>>();
+            List<List<double>> rangbiglist = new List<List<double>>();
+            List<numkol> ranglist = new List<numkol>();
+            List<int> sortlist = new List<int>();
+
+            List<double> rangsr = new List<double>();
+            List<double> rangsum = new List<double>();
+
+            struct numkol
+            {
+                public int num;
+                public double ran;
+
+                  public numkol (int nu, double ra)
+                {
+                    num = nu;
+                    ran = ra;
+                }
+            }
+
+            public List<List<int>> GetList ()
+            {
+                return list;
+            }
+
+            public List<List<double>> GetRangList()
+            {
+                return rangbiglist;
+            }
+            public void TableBuild( DataGridView table, int width)
+            {
+                DataGridViewTextBoxColumn col0;
+               // n = num; k = kol;
+                for (int i = 0; i < k; i++)
+                {
+                    col0 = new DataGridViewTextBoxColumn();
+                    col0.Width = width;
+                    col0.HeaderText = Convert.ToString(i+1);
+                    table.Columns.Add(col0);
+                }
+
+                table.Rows.Add(n);
+                for (int i = 0; i < n; i ++)
+                {
+                        table.Rows[i].HeaderCell.Value = Convert.ToString(i + 1);
+                        table.Rows[i].Height = 20; 
+                }
+                
+            }
+
+            public void TableFill(DataGridView table)
+            {
+                for (int i = 0; i<n; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                        table.Rows[i].Cells[j].Value = list[i][j];
+                }
+            }
+
+            public void TableRangFill(DataGridView table)
+            {
+                table.Rows.Add(2);
+                table.Rows[n].HeaderCell.Value = "Rj";
+                table.Rows[n + 1].HeaderCell.Value = "rj";
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                        table.Rows[i].Cells[j].Value = Math.Round(rangbiglist[i][j], 3);
+                }
+                double temp;
+                for (int i=0; i<k; i++)
+                {
+                    temp = new double();
+                    temp = 0;
+                    for (int j=0; j<n; j ++)
+                    {
+                        temp += rangbiglist[j][i];
+                    }
+                    rangsum.Add(temp);      
+                    rangsr.Add(temp / n);
+                    table.Rows[n].Cells[i].Value = Math.Round(temp, 3);
+                    table.Rows[n+1].Cells[i].Value = Math.Round(temp/n, 3);
+                    rangs += temp;
+                }
+
+            }
+
+            public void FileRead(string filepath)
+            {
+                bool reading = true;
+                string[] temp = new string[255];
+                List<int> templist = new List<int>();
+                try
+                {
+                    temp = File.ReadAllLines(filepath);
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("File not found");
+                }
+
+                int key = 0;
+                bool enterance = false;
+                foreach (string x in temp)
+                {             
+                    templist = new List<int>();
+                    foreach (string z in x.Split(' '))
+                    {
+                        if (key == 2)
+                        { templist.Add(Convert.ToInt32(z)); enterance = true; }
+                        else if (key == 1) { k = Convert.ToInt32(z); key++; }
+                        else { n = Convert.ToInt32(z); key++; }
+                    }
+                    if (enterance == true)
+                    {
+                        list.Add(templist);
+                        sortlist.AddRange(templist);
+                    }
+                }
+                sortlist.Sort();
+            }
+
+            public void Rang ()
+            {
+                int kol = 0;
+                double place = 0;  
+                int num = sortlist[0];
+                for (int i =0; i<sortlist.Count; i++)
+                {
+                    if (num == sortlist[i]) { kol++; place += i + 1; }
+                    else { ranglist.Add(new numkol(num, place/kol)); num = sortlist[i]; kol = 1; place = i+1; }
+                }
+                ranglist.Add(new numkol(num, place/kol));
+                Ranglistfill();
+            }
+
+            private void Ranglistfill()
+            {
+                int temp;
+                int z;
+                List<double> tlist;
+                for (int i = 0; i < n; i++)
+                {
+                    tlist  = new List<double>();
+                    for (int j = 0; j < k; j++)
+                    {
+                        temp = list[i][j];
+                        z = 0;
+                       while (ranglist[z].num != temp)
+                          z++;
+                        tlist.Add(ranglist[z].ran);
+                    }
+                    rangbiglist.Add(tlist);
+                }
+            }
+
+            
+        }
+
+        Factor fact = new Factor();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Enabled = false;
+            Factor fact = new Factor();
+            fact.FileRead(textBox3.Text);
+            fact.TableBuild(table, 40);
+            button2.Enabled = true; button2.Visible = true;
+            button1.Enabled = false;
+            fact.TableFill(table);
+            table2.Visible = true;
+            fact.TableBuild(table2, 40);
+            fact.Rang();
+            fact.TableRangFill(table2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
